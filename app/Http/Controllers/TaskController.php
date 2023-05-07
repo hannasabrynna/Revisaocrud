6cd2dab6369b55a83531a18fac3e81dc4e53e449
hannasabrynna <hannasabrynna135@gmail.com>
2023-05-06 22:11:28 -0300

    Front-end editado e crud completo

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,20 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
+use App\Models\Us;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
 
 class TaskController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+
 
     /**
      * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
@@ -25,14 +25,27 @@ class TaskController extends Controller
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+
+        Task::create([
+            'description' =>  $request ->description,
+            'expiration'  =>  $request ->expiration,
+            'user_id' => Auth::user()->id
+        ]);
+
+        return redirect('/dashboard');
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
      */
     public function show(Task $task)
     {
@@ -41,6 +54,9 @@ class TaskController extends Controller
 
     /**
      * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
      */
     public function edit(Task $task)
     {
@@ -49,17 +65,36 @@ class TaskController extends Controller
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Task $task)
     {
-        //
+
+        $task->update([
+            'description' => $request-> description,
+            'expiration' => $request-> expiration
+        ]);
+
+
+        // $task-> description = $request->description;
+        // $task-> expiration = $request->expiration;
+        // $task->save();
+
+        return redirect ('dashboard');
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+        return redirect('/dashboard');
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,100 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+
+                    <h1 class="text-xl font-semibold">TO DO LIST</h1>
+
+
+                    <fieldset class="border p-2 mb-2 border-pink-500 rounded">
+                        <legend class="text-xl px-2 border rounded-md border-violet-700">Make News Tasks</legend>
+
+                        <form action="{{ route('task.store') }}" method="POST" class="mt-2">
+                            @csrf
+
+                            <div class="mt-4">
+                                <x-input-label for="description" :value="__('Description')" />
+                                <x-text-input id="description" class="block mt-1 w-full " type="text" name="description" required />
+                            </div>
+
+                            <div class="mt-4">
+                                <x-input-label for="expiration" :value="__('Expiration')" />
+                                <x-text-input id="expiration" class="block mt-1 w-full" type="text" name="expiration" required />
+                            </div>
+                            <br>
+                            <x-primary-button class="w-full bg-rose-600"> Add </x-primary-button>
+                        </form>
+                    </fieldset>
+
+                    <br>
+                    @foreach (Auth::user()->myTasks as $task)
+
+                    <div class="flex justify-between border-b mb-2 gap-4
+                    hover:bg-pink-200" x-data=" { showDelete: false, showEdit: false  } ">
+
+                        <div class="flex justify-between flex-grow">
+                            <div>Description: {{ $task -> description }} </div>
+                            <div>Expiration: {{ $task -> expiration }} </div>
+                            <hr>
+                        </div>
+
+                        <div class="flex gap-2">
+                            <div>
+                                <span class="cursor-pointer px-2 bg-pink-900 text-white" @click="showDelete = true "> Delete </span>
+                            </div>
+                            <div>
+                                <span class="cursor-pointer px-2 bg-violet-500 text-white" @click="showEdit = true "> Edit </span>
+                            </div>
+                        </div>
+
+                        <template x-if="showDelete">
+                            <div class="absolute top-0 button-0 left-0 right-0 bg-gray-800 bg-opacity-20 z-0">
+                                <div class="w-96 bg-pink-200 p-4 absolute left-1/4 right-1/4 top-1/4 z-10 ">
+
+                                    <h2 class="text-xl font-bold text-center">Are you sure you want to delete this task?</h2>
+
+                                    <div class="flex justify-between mt-4">
+                                        <form action="{{  route('task.destroy', $task) }}" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <x-danger-button class="bg-pink-500" > Delete </x-danger-button>
+                                        </form>
+                                        <x-primary-button class="bg-violet-900" @click="showDelete = false"> Cancel </x-primary-button>
+                                    </div>
+                                </div>
+                            </div>
+                        </template>
+
+                        <template x-if="showEdit">
+                            <div class="absolute top-0 button-0 left-0 right-0 bg-gray-800 bg-opacity-20 z-0">
+                                <div class="w-96 bg-white p-4 absolute left-1/4 right-1/4 top-1/4 z-10 ">
+
+                                    <h2 class="text-xl font-bold text-center">{{ $task->description }}</h2>
+                                    
+                                    <form class="my-4" action="{{  route('task.update', $task) }}" method="POST">
+                                        @csrf
+                                        @method('PUT')
+                                        <x-text-input name="description" placeholder="Descrição" value="{{ $task->description }}" required></x-text-input>
+                                        <x-text-input name="expiration" placeholder="Expiração" value="{{ $task->expiration }}" required></x-text-input>
+                                        <x-primary-button> Edit </x-primary-button>
+                                    </form>
+                                    <x-primary-button @click="showEdit = false" class="w-full"> Cancel </x-primary-button>
+                                </div>
+                            </div>
+                        </template>
+                    </div>
+                    @endforeach
+
                 </div>
             </div>
         </div>
+    </div>
     </div>
 </x-app-layout>
